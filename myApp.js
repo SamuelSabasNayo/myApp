b663bbc0b45c6683a618ce99a8bc372aa8286eb4
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-12-27 13:03:56 +0200

    Added changes

---
+++ myApp.js
@@ -0,0 +1,16 @@
+const alive = true,
+    eat = () => console.log('To eat food.'),
+    sleep = () => console.log('To lie on a bed.'),
+    code = () => console.log('To write a friendly language.'),
+    repeat = () => console.log('To do it many times.');
+
+
+
+
+
+while (alive) {
+    eat();
+    // sleep();
+    code();
+    repeat();
+};
